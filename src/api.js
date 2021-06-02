b347cbe1bffd23d8bcb9b7f788ea9baefe934a51
MarvinKweyu <mkweyu1@gmail.com>
2021-06-02 12:23:41 +0300

    chore: create fetch function for user detail

--- src/api.js
+++ src/api.js
@@ -1,1 +1,18 @@
-export default {}
+import axios from 'axios'
+import httpAdapter from 'axios/lib/adapters/http'
+
+
+const instance = axios.create({
+  baseURL: 'https://api.github.com',
+  // pass an adapter to allow use of axios with nock
+  // let nock intercept requests
+  adapter: httpAdapter,
+})
+
+export default {
+    searchUser(username) {
+    return instance
+      .get(`/users/${username}`)
+      .then(result => result.data)
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+const UserView = () => import('@/views/UserView')
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    component: UserView
+  }
+];
 
 const router = new VueRouter({
   mode: "history",
